54a6172278b60bcd0763f96e5507958d708643bd
shahbaz-kamal <shahbazkamal384@gmail.com>
2024-09-21 01:21:38 +0600

    cash out partialy done

---
+++ js/cash-out.js
@@ -0,0 +1,29 @@
+// Step 1: add event listener
+document
+  .getElementById("btn-cash-out")
+  .addEventListener("click", function (event) {
+    // step 2: prevent Default loading
+    event.preventDefault();
+    // step 3: get pin & current balance
+    const balanceCashOut = document.getElementById(
+      "money-to-be-cashed-out"
+    ).value;
+    const verifyPinCashOut = document.getElementById(
+      "pin-to-verify-cash-out"
+    ).value;
+    // Step 4: Verify Pin
+    if (verifyPinCashOut === "1234") {
+      //   step 5: Subtracting balance
+      const currentBalance =
+        document.getElementById("current-balance").innerText;
+      const currentBalanceNumber = parseFloat(currentBalance);
+      const moneyToBeSubtractedNumber = parseFloat(balanceCashOut);
+      const newBalanceCashOut =
+        currentBalanceNumber - moneyToBeSubtractedNumber;
+
+      //   step 6: Updating the balance
+      document.getElementById("current-balance").innerText = newBalanceCashOut;
+    } else {
+      prompt("Wrong pin");
+    }
+  });
--- js/home.js
+++ js/home.js
@@ -1,10 +1,22 @@
-console.log("Home addede");
 // step 1: Add an event handler button inside the form
 document
   .getElementById("btn-add-money")
   .addEventListener("click", function (event) {
     // step 2: Prevent page reload after form submit
     event.preventDefault();
+    // step3 : get money to be added & verify pin
     const moneyToBeAdded = document.getElementById("money-to-be-added").value;
     const pinToVerify = document.getElementById("pin-to-verify").value;
+    if (pinToVerify === "1234") {
+      //   step 4: get the current balance
+      const balance = document.getElementById("current-balance").innerText;
+      // ste5 5:add moneyToBeAdded with currentBalance
+      const addMoneyNumber = parseFloat(moneyToBeAdded);
+      const balanceNumber = parseFloat(balance);
+      const newBalance = addMoneyNumber + balanceNumber;
+      //   Stet 6: showing the total balance
+      document.getElementById("current-balance").innerText = newBalance;
+    } else {
+      alert("Wrong pin");
+    }
   });
